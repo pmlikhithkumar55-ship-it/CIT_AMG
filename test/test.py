4318c4b1293a3efffa74b8122166230657ca7a89
PM LIKHITH KUMAR <pmlikhithkumar55@gmail.com>
2026-05-25 23:19:13 +0530

    Updated Cocotb verification script

--- test/test.py
+++ test/test.py
@@ -1,40 +1,38 @@
-# SPDX-FileCopyrightText: © 2024 Tiny Tapeout
-# SPDX-License-Identifier: Apache-2.0
-
 import cocotb
-from cocotb.clock import Clock
-from cocotb.triggers import ClockCycles
-
+from cocotb.triggers import Timer
 
 @cocotb.test()
-async def test_project(dut):
-    dut._log.info("Start")
+async def ecc_accelerator_test(dut):
 
-    # Set the clock period to 10 us (100 KHz)
-    clock = Clock(dut.clk, 10, unit="us")
-    cocotb.start_soon(clock.start())
-
-    # Reset
-    dut._log.info("Reset")
-    dut.ena.value = 1
     dut.ui_in.value = 0
     dut.uio_in.value = 0
+    dut.ena.value = 1
     dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 10)
+
+    await Timer(10, units="ns")
+
     dut.rst_n.value = 1
 
-    dut._log.info("Test project behavior")
+    # Test Case 1
+    dut.ui_in.value = 5
+    dut.uio_in.value = 3
 
-    # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
+    await Timer(20, units="ns")
 
-    # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 1)
+    print("Output 1:", dut.uo_out.value)
 
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
+    # Test Case 2
+    dut.ui_in.value = 10
+    dut.uio_in.value = 2
 
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
+    await Timer(20, units="ns")
+
+    print("Output 2:", dut.uo_out.value)
+
+    # Test Case 3
+    dut.ui_in.value = 15
+    dut.uio_in.value = 5
+
+    await Timer(20, units="ns")
+
+    print("Output 3:", dut.uo_out.value)
